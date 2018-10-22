e0d4fd23ad984fd19faf764dc7c32a1e47658770
Tom Rowson <tom.rowson@gmail.com>
2018-10-22 18:48:48 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,6 +49,7 @@ def monopoly_with_third_tier
 	      three_pieces_owned: 100,
 	      four_pieces_owned: 200
 	    }
+	  
 	  }
 	}
 		
@@ -78,7 +79,7 @@ def monopoly_with_fourth_tier
 	      three_pieces_owned: 100,
 	      four_pieces_owned: 200
 	    }
-	    
+	  }  
 	  }
 	  
 	}
