a8b892ae3b88d5d6273a31880e74c88c0a9fca79
Tom Rowson <tom.rowson@gmail.com>
2018-10-22 18:52:10 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,9 +39,9 @@ def monopoly_with_third_tier
 	    pieces: 4,
 	    names: {
 	      reading_railroad: {},
-	      pennsylvania_railroad = {},
-	      b_and_o_railroad = {},
-	      shortline = {}
+	      pennsylvania_railroad: {},
+	      b_and_o_railroad: {},
+	      shortline: {}
 	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
@@ -63,13 +63,13 @@ def monopoly_with_fourth_tier
 	      reading_railroad: {
 	        "mortgage_value" => "$100",
 	      },
-	      pennsylvania_railroad = {
+	      pennsylvania_railroad: {
 	        "mortgage_value" => "$200"
 	      },
-	      b_and_o_railroad = {
+	      b_and_o_railroad: {
 	        "mortgage_value" => "$400"
 	      },
-	      shortline = {
+	      shortline: {
 	        "mortgage_value" => "$800"
 	      }
 	    },
